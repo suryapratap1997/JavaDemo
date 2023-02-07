7c289b654fa050aa761309414cb8e69b27d65034
suryapratap1997 <spsrcse@gmail.com>
2023-02-07 22:54:17 +0530

    Data type first

---
+++ DataType.java
@@ -0,0 +1,26 @@
+class DataType
+
+{
+
+public static void main(String args[])
+
+{
+
+int i=112500;
+byte b=125;
+short s=13354;
+long l=5522;
+doubt d=52525.25;
+float f=1212.5;
+boolean b=true;
+System.out.println(i + " "+ b " "+ s " "+ l " "+ d " "+ f " "+ b " ");
+
+
+
+}
+
+
+
+
+
+}
